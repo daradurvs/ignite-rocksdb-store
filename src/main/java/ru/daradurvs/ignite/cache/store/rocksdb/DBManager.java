7bfa374ee64f77b0b73f5ecec78f127eba9c6e88
Vyacheslav Daradur <daradurvs@gmail.com>
2017-10-26 15:36:39 +0300

    wip

--- src/main/java/ru/daradurvs/ignite/cache/store/rocksdb/DBManager.java
+++ src/main/java/ru/daradurvs/ignite/cache/store/rocksdb/DBManager.java
@@ -1,19 +1,56 @@
 package ru.daradurvs.ignite.cache.store.rocksdb;
 
-import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.rocksdb.ColumnFamilyDescriptor;
 import org.rocksdb.ColumnFamilyHandle;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
-import org.rocksdb.RocksIterator;
 
 /**
  * @author Vyacheslav Daradur
  * @since 25.10.2017
  */
 public class DBManager implements AutoCloseable {
+    private static final String PATH_TO_DATABASE = "c:/TEMP/rocksdb";
+    private static final Map<byte[], ColumnFamilyHandle> COLUMN_FAMILY_HANDLES = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        try (Options options = new Options()) {
+            options.setCreateIfMissing(true);
+
+            List<byte[]> columnFamilyNames = RocksDB.listColumnFamilies(options, PATH_TO_DATABASE);
+
+            List<ColumnFamilyDescriptor> descriptors = new ArrayList<>();
+
+            for (byte[] name : columnFamilyNames) {
+                descriptors.add(new ColumnFamilyDescriptor(name));
+            }
+
+            List<ColumnFamilyHandle> handles = new ArrayList<>();
+
+            try (RocksDB db = RocksDB.open(PATH_TO_DATABASE, descriptors, handles)) {
+                assert descriptors.size() == handles.size();
+
+                for (int i = 0; i < descriptors.size(); i++) {
+                    byte[] name = descriptors.get(i).columnFamilyName();
+
+                    COLUMN_FAMILY_HANDLES.put(name, handles.get(i));
+                }
+
+                for (byte[] bytes : COLUMN_FAMILY_HANDLES.keySet()) {
+                    System.out.println(new String(bytes));
+                    System.out.println(COLUMN_FAMILY_HANDLES.get(bytes));
+                }
+            }
+        }
+    }
+
+
+
     private static RocksDB db;
 
     public static RocksDB db() throws RocksDBException {
@@ -22,16 +59,6 @@ public class DBManager implements AutoCloseable {
 
         return db;
     }
-//
-//    public static ColumnFamilyHandle getColumnDescriptor() throws IOException, RocksDBException {
-//        final String name = "handlerName";
-//
-//        ColumnFamilyHandle handle = null;
-//
-//        byte[] arr = new JavaSerializer().serialize("handlerName");
-//
-//        return new ColumnFamilyDescriptor();
-//    }
 
     @Override public void close() throws Exception {
         if (db != null)
