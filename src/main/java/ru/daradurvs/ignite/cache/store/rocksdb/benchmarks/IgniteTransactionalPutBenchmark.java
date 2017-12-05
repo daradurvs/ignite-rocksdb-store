46180cfe2e78f80c760c9332772f63061f353978
Vyacheslav Daradur <daradurvs@gmail.com>
2017-12-05 15:12:17 +0300

    fixed pds config

--- src/main/java/ru/daradurvs/ignite/cache/store/rocksdb/benchmarks/IgniteTransactionalPutBenchmark.java
+++ src/main/java/ru/daradurvs/ignite/cache/store/rocksdb/benchmarks/IgniteTransactionalPutBenchmark.java
@@ -1,12 +1,17 @@
 package ru.daradurvs.ignite.cache.store.rocksdb.benchmarks;
 
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.configuration.DataStorageConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.yardstick.cache.IgnitePutBenchmark;
 
 /** {@inheritDoc} */
 public class IgniteTransactionalPutBenchmark extends IgnitePutBenchmark {
     /** {@inheritDoc} */
     @Override protected IgniteCache<Integer, Object> cache() {
+        if (!ignite().active())
+            ignite().active(true);
+
         return ignite().cache("transactional");
     }
 }
